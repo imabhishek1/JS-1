9a13b4d6209ef23e630704dbfd1a786ff9dbc0b8
imabhishek1 <shekabhi259@gmail.com>
2023-09-03 15:18:54 +0000

    let const and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "abhi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" 
+
+
+// accountId = 2 // Not Allowed
+
+accountEmail = "hsbc@gmial.com"
+accountPassword = "45678"
+accountCity = "Delhi"
+let accountState;
+
+console.log(accountId);
+
+/*
+    Prefer not to use var 
+    because of issue in block scope and functional scope.
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity.accountState])
