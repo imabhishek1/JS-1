40f1b3719aed7f67433038e122e5009cd6beb508
imabhishek1 <shekabhi259@gmail.com>
2023-09-09 20:28:50 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,39 @@
+const name = "abhish_k"
+const repoCount =  50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repoCount is  ${repoCount}`);
+
+
+const gameName = new String('abhishek_hc_com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(5))
+// console.log(gameName.indexOf('s'))
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);  //abhi 
+
+// const anotherString = gameName.slice(-7,4)
+// console.log(anotherString);
+
+
+const newString1 = "         hitesh   "
+console.log(newString1);
+// console.log(newString1.trim());  // trim starting place and end space  // trim only works on space and line terminators
+
+const url = "https://hitesh.com/hitesh%20choudary"
+
+console.log(url.replace("%20", "-")); 
+
+console.log(url.includes('abhi'));
+
+
+//convert string into array
+
+console.log(gameName.split('_'));
